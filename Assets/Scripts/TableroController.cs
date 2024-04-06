a2ac658f4699d5cf2f06c70f58dfff1e345672f7
Mateo Cabrera <84540061+m-cabrerar@users.noreply.github.com>
2024-04-06 04:27:33 -0300

    Inicializo scripts

---
+++ Assets/Scripts/TableroController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableroController : MonoBehaviour
+{
+    [SerializeField] GameObject[] Casillas;
+    [SerializeField] GameObject[] Jugadores;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
