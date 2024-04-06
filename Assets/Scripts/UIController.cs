10aa843b4bb4cb5827cf18de19c94c655d4ee60d
FSendot <fsendot@itba.edu.ar>
2024-04-06 10:00:30 -0300

    Make basic turn flow

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    private void FinishTurn() {
+        turno++;
+        DiceController.EnableDice();
+    }
+}
