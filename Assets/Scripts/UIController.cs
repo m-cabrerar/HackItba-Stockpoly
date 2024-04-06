a613564272d8373b23459204c8f764f4ed001e81
Mateo Cabrera <84540061+m-cabrerar@users.noreply.github.com>
2024-04-06 19:45:04 -0300

    tarjetas

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -10,10 +10,20 @@ public class UIController : MonoBehaviour
     [SerializeField] public Button TerminarTurnoButton;
     [SerializeField] CasillaController[] casillas;
 
+    [System.Serializable] public struct TarjetaUI 
+    { 
+        public TextMeshProUGUI nombre;
+        public Image image;
+        public TextMeshProUGUI detalle;
+        public TextMeshProUGUI precio;
+    }
     
+    [SerializeField] TarjetaUI tarjeta;
+
     void Start()
     {
         TriggerButton(false);
+        tarjetaEnable(false);
     }
 
     public void RenderPlayerContext(PlayerController player) // ejecutar al inicio de un turno
@@ -34,16 +44,29 @@ public class UIController : MonoBehaviour
     public void RenderNewTurnContext()
     {
         TriggerButton(false);
+        tarjetaEnable(false);
     }
 
     public void RenderTarjeta(ItemData data)
     {
-        
+        tarjetaEnable(true);
+        tarjeta.nombre.text = data.nombre;
+        tarjeta.image.GetComponent<Image>().sprite = data.sprite;
+        tarjeta.detalle.text = data.detalle;
+        tarjeta.precio.text = "$" + data.precio.precioBase * (1 + data.precio.variacion);
     }
 
     private void TriggerButton(bool state)
     {
         TerminarTurnoButton.gameObject.SetActive(state);
-        TerminarTurnoButton.enabled = state;    
+        TerminarTurnoButton.enabled = state;   
+    }
+
+    private void tarjetaEnable(bool state)
+    {
+        tarjeta.nombre.enabled = state;
+        tarjeta.detalle.enabled = state;
+        tarjeta.image.enabled = state;
+        tarjeta.precio.enabled = state;
     }
 }
